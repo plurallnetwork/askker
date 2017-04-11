f301a301d69ca85c75adba462a0bbd73260bc79d
Leandro Barbieri <leandrobarbieri21@gmail.com>
2017-04-11 20:10:38 +0200

    ASKKER-195 - Create setNotification internal webAPI method.

---
+++ Askker.App.PortableLibrary/Models/UserNotificationModel.cs
@@ -0,0 +1,26 @@
+﻿using Askker.App.PortableLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Askker.App.PortableLibrary.Models
+{
+    public class UserNotificationModel
+    {
+        public string userId { get; set; }
+
+        public string notificationDate { get; set; }
+
+        public UserFriendModel notificationUser { get; set; }
+
+        public string type { get; set; }
+
+        public string text { get; set; }
+
+        public string link { get; set; }
+
+        public int isDismissed { get; set; }
+    }
+}
