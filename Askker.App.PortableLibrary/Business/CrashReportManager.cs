fd27371c0b10a7b0b05457e4927b81ff23a5de42
Alex Tubone <tubbonne@gmail.com>
2018-04-04 16:23:14 -0300

    ASKKER-678 - Create an error handler

---
+++ Askker.App.PortableLibrary/Business/CrashReportManager.cs
@@ -0,0 +1,37 @@
+﻿using Askker.App.PortableLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Askker.App.PortableLibrary.Business
+{
+    public class CrashReportManager
+    {
+        public CrashReportService crashReportService { get; set; }
+
+        public CrashReportManager()
+        {
+            crashReportService = new CrashReportService();
+        }
+
+        public void PostCrashReport(string authenticationToken, string report)
+        {
+            var response = crashReportService.PostCrashReport(authenticationToken, report);
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    throw new Exception("Unauthorized");
+                }
+                else
+                {
+                    throw new Exception(response.StatusCode.ToString() + " - " + response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ Askker.App.PortableLibrary/Services/CrashReportService.cs
@@ -0,0 +1,34 @@
+﻿using Askker.App.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Askker.App.PortableLibrary.Services
+{
+    public class CrashReportService
+    {
+        public HttpResponseMessage PostCrashReport(string authenticationToken, string report)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string boundary = "---8d0f01e6b3b5dafaaadaad";
+                    var content = new MultipartFormDataContent(boundary);
+                    content.Add(new StringContent(report, Encoding.UTF8, "text/plain"), "report");
+                    
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Bearer " + authenticationToken);
+
+                    return client.PostAsync(EnvironmentConstants.getServerUrl() + "api/Account/CrashReport", content).Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
